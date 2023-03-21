f442a7c474f7788458b5e18ffc054dae14166e71
anhn <anhnd85@gmail.com>
2023-03-22 00:24:44 +0100

    make it more simple to test
    st.set_page_config(
        page_title="Ex-stream-ly Cool App",
        page_icon="🧊",
        layout="wide",
        initial_sidebar_state="expanded",
        menu_items={
            'About me': 'https://www.usn.no/english/about/contact-us/employees/anh-nguyen-duc',
            'Service': "# This is the very first version of your Startup Virtual Assistant"
        }
    
    
    
    col1, col2= st.columns((1,3))
    col1.subheader('Software Project Management')
    col2.subheader('Your project assistant')
    with col1:
        st.image('pmmethod.png')
    
    with col2:

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,6 @@ from streamlit_chat import message
 
 openai.api_key =  "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"
 
-st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'About me': 'https://www.usn.no/english/about/contact-us/employees/anh-nguyen-duc',
-        'Service': "# This is the very first version of your Startup Virtual Assistant"
-    }
-)
-
 def generate_response(prompt):
     completions = openai.Completion.create(
         engine = "text-davinci-003",
@@ -32,35 +21,28 @@ def generate_response(prompt):
 #Creating the chatbot interface
 st.title("Your virtual assistant")
 
-col1, col2= st.columns((1,3))
-col1.subheader('Software Project Management')
-col2.subheader('Your project assistant')
-with col1:
-    st.image('pmmethod.png')
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
 
-with col2:
-    # Storing the chat
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = []
+# We will get the user's input by calling the get_text function
+def get_text():
+    input_text = st.text_input("You: ","What is meaning of a human life?", key="input")
+    return input_text
 
-    # We will get the user's input by calling the get_text function
-    def get_text():
-        input_text = st.text_input("You: ","What is meaning of a human life?", key="input")
-        return input_text
+user_input = get_text()
 
-    user_input = get_text()
+if user_input:
+    output = generate_response(user_input)
+    # store the output 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
 
-    if user_input:
-        output = generate_response(user_input)
-        # store the output 
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(output)
+if st.session_state['generated']:
 
-    if st.session_state['generated']:
-        
-        for i in range(len(st.session_state['generated'])-1, -1, -1):
-            message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')

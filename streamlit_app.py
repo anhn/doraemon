d54c0c28a781cc4d3a17b13cc9a3a341db4221e9
anhn <anhnd85@gmail.com>
2023-03-22 10:24:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def get_text():
 
 jim_line = get_text()
 
-st.text(get_response(jim_line))
+st.markdown(get_response(jim_line))
 #if jim_line:
 #    output = get_response(jim_line)
 #    # store the output 

c898eb3e2ab9bdc24cd96e2b727edcbe2f9f2013
anhn <anhnd85@gmail.com>
2023-03-22 11:10:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ def get_text():
 
 jim_line = get_text()
 
-st.markdown(""" :mailbox: Mc Cathy:    """ + get_response(jim_line))
+st.markdown(""" :mailbox: Mc Cathy:    \ """ + get_response(jim_line))
 #if jim_line:
 #    output = get_response(jim_line)
 #    # store the output 

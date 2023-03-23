de388432f7ed550f145598d5be0fabb63853fd0e
anhn <anhnd85@gmail.com>
2023-03-23 14:38:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,45 +55,43 @@ if 'past' not in st.session_state:
 
 def get_text():
     input_text = st.text_area("Say something to Hannah:", height=10, key='option')
-    stt_button = Button(label="Speak", width=100)
-    stt_button.js_on_event("button_click", CustomJS(code="""
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
-
-        recognition.onresult = function (e) {
-            var value = "";
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                    value += e.results[i][0].transcript;
-                }
-            }
-            if ( value != "") {
-                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-            }
-        }
-        recognition.start();
-        """))
-    result = streamlit_bokeh_events(
-        stt_button,
-        events="GET_TEXT",
-        key="listen",
-        refresh_on_update=False,
-        override_height=75,
-        debounce_time=0)
-    if result:
-        if "GET_TEXT" in result:
-            st.session_state['options'] = result.get("GET_TEXT")
-            st.write(result.get("GET_TEXT"))
     return input_text
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+stt_button = Button(label="Speak", width=100)
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+if result:
+    if "GET_TEXT" in result:
+        st.session_state['options'] = result.get("GET_TEXT")
+        st.write(result.get("GET_TEXT"))
+        
 jim_line = get_text()
-
-
-
 
 st.session_state.past.append(jim_line)
 cathy_line =  get_response(st.session_state['past'][-1] + jim_line)

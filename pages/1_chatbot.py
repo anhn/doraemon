24bbfbad1bdf5c8ee0382cc0284b013edfe6829e
anhn <anhnd85@gmail.com>
2023-03-25 23:33:36 +0100

    Update 1_chatbot.py

--- pages/1_chatbot.py
+++ pages/1_chatbot.py
@@ -133,8 +133,8 @@ translator = Translator()
 #elif out_lang == "Vietnamese":
 #    output_language = "vn"
 
-input_lang == "vn"
-output_lang == "vn"
+input_language = "vn"
+output_language = "vn"
 tld = "com"
 
 def text_to_speech(input_language, output_language, text, tld):

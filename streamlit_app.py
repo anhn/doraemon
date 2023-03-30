7d45d5762be414f1cc51812959f1d35472a98632
anhn <anhnd85@gmail.com>
2023-03-30 14:21:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,19 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Hannah's space! 👋")
+st.write("# Welcome to my learning space! 👋")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    As a researcher who believes in the power of automation in education, my experiment space with chatbots has been a fascinating journey. By leveraging the advancements in artificial intelligence and natural language processing, I have been exploring the potential of chatbots as an effective tool for automating learning and teaching processes. With the ability to engage learners in personalized conversations, provide instant feedback, and offer customized learning paths, chatbots have the potential to transform the traditional classroom experience. My research aims to identify the most effective ways to design and implement chatbots in education to enhance the learning outcomes and create a more accessible and inclusive learning environment for all.
+    You can findout more about me:
+    Linkedin: https://www.linkedin.com/in/anhnguyenduc/
+    Google Scholar: https://scholar.google.no/citations?user=ia02J_oAAAAJ&hl=no
+    My current affliation: 
+        USN: https://www.usn.no/english/about/contact-us/employees/anh-nguyen-duc
+        NTNU: https://www.ntnu.no/ansatte/anhn
+        Fulbright: https://fulbright.edu.vn/our-team/nguyen-duc-anh/
 """
 )

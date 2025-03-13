9823c589039323f153c46a13431fdf234a519665
anhn <anhnd85@gmail.com>
2025-03-13 08:23:43 +0100

    Create 2_Chỉnh_sửa_Chatlog.py

---
+++ pages/2_Chỉnh_sửa_Chatlog.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import os
+from pymongo import MongoClient
+
+st.set_page_config(layout="wide") 
+
+# Load MongoDB URI from secrets
+MONGO_URI = st.secrets["mongo"]["uri"]  
+DB_NAME = "utt_detai25"
+FAQ_COLLECTION = "chatlog"
+
+# Connect to MongoDB
+client_mongo = MongoClient(MONGO_URI)
+db = client_mongo[DB_NAME]
+faq_collection = db[FAQ_COLLECTION]
+
+st.write("# Admin Page for UTT Tuyển Sinh 👋")
+
+# Load existing FAQ data from MongoDB
+faqs = list(faq_collection.find({}, {"_id": 0}).limit(30))
+faq_df = pd.DataFrame(faqs)
+
+if faq_df.empty:
+    st.warning("No Chatlog data found in the database.")
+else:
+    st.subheader("Editable Chatlog Data")
+    edited_df = st.data_editor(faq_df, num_rows="dynamic")
+
+    # Button to save changes to MongoDB
+    if st.button("Save Changes to Database"):
+        if not edited_df.empty:
+            faq_collection.delete_many({})  # Clear existing data
+            faq_collection.insert_many(edited_df.to_dict(orient="records"))
+            st.success("Database updated successfully!")
+        else:
+            st.warning("No data to save!")
